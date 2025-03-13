f2bbdac04257f69254406eed2ea16e376e6f328a
Montfld04 <ivet.flores7968@alumnos.udg.mx>
2025-03-12 20:02:59 -0600

    Update array.py

--- tempoFor.py
+++ tempoFor.py
@@ -1,11 +1,8 @@
-import time
-'''Libreria time proporciona opciones para lograr una manipulacion del tiempo'''
-#hare una prueba con workflows
-for hora in range (24):
-    for min in range (60):
-        for seg in range (60):
-            print ("Hora", hora, "minutos", min, "segundos", seg)
-            print (hora, ":", min, ":", seg)
-            time.sleep(1)
-            #la funcion time.sleep() hace pausas segun el tiempo especificado
-            #para poder hacer una visualizacion mejor del tiempo
+import numpy as np
+
+a = np.array([[1,2,7,4], [5,6,7,8], [9,10,11,12]])
+
+print(a[a<=5])
+'''.ndim muestra la dimension que tiene un arreglo'''
+b=a.ndim 
+print(b)
