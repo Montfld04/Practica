130a3ff9cd741ae406e0ae2f6cbf22c49abb9779
Montfld04 <ivet.flores7968@alumnos.udg.mx>
2025-03-12 19:52:41 -0600

    Update tempoFor.py

--- tempoFor.py
+++ tempoFor.py
@@ -1,6 +1,6 @@
 import time
 '''Libreria time proporciona opciones para lograr una manipulacion del tiempo'''
-
+#hare una prueba con workflows
 for hora in range (24):
     for min in range (60):
         for seg in range (60):
