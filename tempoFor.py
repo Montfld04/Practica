45edb462148c49601757d123ea0d635586ff7c70
Montfld04 <ivet.flores7968@alumnos.udg.mx>
2025-03-12 19:40:17 -0600

    Add files via upload

---
+++ tempoFor.py
@@ -0,0 +1,11 @@
+import time
+'''Libreria time proporciona opciones para lograr una manipulacion del tiempo'''
+
+for hora in range (24):
+    for min in range (60):
+        for seg in range (60):
+            print ("Hora", hora, "minutos", min, "segundos", seg)
+            print (hora, ":", min, ":", seg)
+            time.sleep(1)
+            #la funcion time.sleep() hace pausas segun el tiempo especificado
+            #para poder hacer una visualizacion mejor del tiempo
